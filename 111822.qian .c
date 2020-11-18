01ea787c919699ee4a3730dbfa1f3d30c9019ad7
lanhongjun <74277782+lanhongjun@users.noreply.github.com>
2020-11-18 11:55:56 +0800

    Update 111822.qian .c

--- 111822.qian .c
+++ 111822.qian .c
@@ -1,38 +1,37 @@
 #include <stdio.h>
 void main()
 {int a;
-printf("请问你的工资是多少");
 scanf("%d",&a);
 if(a<0)
 {
-	printf("你还有花呗没还，无法使用该程序。");
+	printf("你还有花呗没还，无法使用该程序");
 }
 
-else if(0<a<500)
+else if(0<=a && a<=500)
 {
 	printf("赤贫");
 }
-else if(a<0)
+else if(501<=a && a<=1500)
 {
 	printf("贫困");
 }
-else if(501<a<1500)
+else if(1501<=a && a<=3000)
 {
-	printf("普通打工人。");
+	printf("普通打工人");
 }
-else if(1501<=a<=3000)
+else if(3000<=a && a<=5000)
 {
 	printf("普通清洁工");
 }
-else if(3001<=a<=5000)
+else if(5001<=a && a<=6000)
 {
 	printf("水电工工资");
 }
-else if(5001<=a<=12000)
+else if(6001<=a && a<=12000)
 {
 	printf("脑袋大脖子粗不是老板就是伙夫");
 }
-else (50001<=a<=200000000);
+else
 {
 printf("程序员");
-}
+}}
