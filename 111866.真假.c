6b0d7e870691576ebecb4da79b264076c144880b
lanhongjun <74277782+lanhongjun@users.noreply.github.com>
2020-11-18 09:16:34 +0800

    Create 111866.真假.c

---
+++ 111866.真假.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+void main()
+{
+int a=3,b=4,c=5,x,y;
+if (a=b>c && b==c)
+{
+	printf("1真\n");
+}
+else 
+{
+printf("1假\n");
+}
+if (a||b+c && b-c)
+{
+	printf("2真\n");
+}
+else 
+{
+printf("2假\n");
+}
+if (!(a>b) && !c||1)
+{
+	printf("3真\n");
+}
+else 
+{
+printf("3假");}
+if (!(x=a) && (y=b) && 0)
+{
+	printf("4真\n");
+}
+else 
+{
+printf("4假\n");}
+if (!(a+b)+c-1 && b+c/2)
+{
+	printf("5真\n");
+}
+else 
+{
+printf("5假\n");
+}
+
+}
