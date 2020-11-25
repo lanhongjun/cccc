15dd194150d7d5b0246b7bb6a12ca6dd1525d2d1
lanhongjun <74277782+lanhongjun@users.noreply.github.com>
2020-11-25 16:59:43 +0800

    Create 1125延迟回复.1.c

---
+++ 1125延迟回复.1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <windows.h>
+void main()
+{
+int a,b;
+for(a=1;a<=9;a++)
+for(b=1;b<=9;b++)
+printf("%dx%d=%d\t",a,b,a*b);
+Sleep(2000);
+printf("										这你都不会，回家种田吧！\n");
+Sleep(2000);
+printf("							你还别不服,我就问问你18x18等于多少两秒你都答不出来");
+Sleep(2000);
+printf("											看看我就说你就是不会");
+}
+
