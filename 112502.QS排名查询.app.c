81c138752db9d7760649034929e9e95f8d68787a
lanhongjun <74277782+lanhongjun@users.noreply.github.com>
2020-11-25 09:47:37 +0800

    Create 112502.QS排名查询.app.c

---
+++ 112502.QS排名查询.app.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+void main()
+{int a;
+scanf("%d",&a);
+if(a==1)
+{
+	printf("广西科技师范学院");
+}
+if(a==2)
+{
+	printf("清华大学");
+}if(a==3)	
+{
+	printf("北京大学");
+}if(a==4)
+{
+	printf("上海交大");
+}if(a==5)
+{
+	printf("复旦大学");
+}if(a==6)
+{
+	printf("浙江大学");
+}if(a==7)
+{
+	printf("中国科技大学");
+}if(a==8)
+{
+	printf("华中科技大学");
+}if(a==9)
+{
+	printf("国防科技大学");
+}if(a==10)
+{
+	printf("深圳大学");
+}
+else
+{
+	printf("老铁的排名太靠后了，请输入1-10");
+}
+}
