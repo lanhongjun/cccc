c15bf16e6bc1f1c7ec47fa19d59725a3d010287c
lanhongjun <74277782+lanhongjun@users.noreply.github.com>
2020-12-02 17:08:40 +0800

    Update 120203.秒转时分秒.app.c

--- 120203.秒转时分秒.app.c
+++ 120203.秒转时分秒.app.c
@@ -1,33 +1,44 @@
 #include<stdio.h>
 #include <windows.h>
 void main()
-{int i,o=0,p=0;
-for(i=0;;i++)
 {
-Sleep(1000);
+	int i,o=0,p=0;
+	for(i=0;;i++)
+{
+Sleep(1);
 printf("%02d\n",i);
 
 
-if(61<i)
+	if(61<i)
 {
 	i=0;
 	o=o+1;
 }
-else
+	else
 {
 	i=i;
 }
 printf("%02d:",p);
 
-if(o>61)
+	if(o>61)
 {
-o=0;
-p=p+1;
+	o=0;
+	p=p+1;
 }
-else
+	else
 {
-o=o;
+	o=o;
 }
-printf("%02d:",o);
+	printf("%02d:",o);
+	if(p==24 && o==59 && i==59)
+{
+	p=0;
+	i=0;
+	o=0;
+}
+	else
+{
+	p=p;
+}
 }
 }
