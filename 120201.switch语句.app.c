03bff17a4914188a2fa4d9ff530699c9652bac36
lanhongjun <74277782+lanhongjun@users.noreply.github.com>
2020-12-02 08:55:23 +0800

    Create 120201.switch语句.app.c

---
+++ 120201.switch语句.app.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+switch(n)
+{
+case 1:printf("清华大学");break;
+case 2:printf("北京大学");break;
+case 3:printf("复旦大学");break;
+case 4:printf("上海交通大学");break;
+case 5:printf("浙江大学");break;
+case 6:printf("中国科学技术大学");break;
+case 7:printf("南京大学");break;
+case 8:printf("北京师范大学");break;
+case 9:printf("武汉大学");break;
+case 10: printf("南开大学");break;
+case 11:printf("哈尔滨工业大学");break;
+case 12:printf("中山大学");break;
+case 13:printf("西安交通大学");break;
+case 14:printf("同济大学");break;
+case 15:printf("北京航天航空大学");break;
+case 16:printf("厦门大学");break;
+case 17:printf("北京理工大学");break;
+case 18:printf("上海大学");break;
+case 19:printf("天津大学");break;
+case 20:printf("人民大学");break;
+default:printf("对不起，你输入的值有误，请输入1-20的数值");
+}
+
+}
