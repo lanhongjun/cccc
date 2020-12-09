7ae8c883c51293a7c1c71c61e7228913066d29fb
lanhongjun <74277782+lanhongjun@users.noreply.github.com>
2020-12-09 16:46:35 +0800

    Create 120904.串比对.app.c

---
+++ 120904.串比对.app.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <string.h>
+void main()
+{
+char s1[]="liuliuliu";
+char s2[]="liuliuliu";
+if (strcmp(s1,s2)==0)
+{
+	printf("正确");
+}
+else
+{
+	printf("不正确");
+}
+
+}
