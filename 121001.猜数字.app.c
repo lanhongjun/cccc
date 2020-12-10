ca38b3e23da226dcc1e74700d7c418364fb9685a
lanhongjun <74277782+lanhongjun@users.noreply.github.com>
2020-12-10 21:15:54 +0800

    Create 121001.猜数字.app.c

---
+++ 121001.猜数字.app.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char a[50];
+	char a1[]="天王盖地虎";
+	char a2[]="宝塔镇河妖";
+	int q,i,o=15;
+	printf("天王盖地虎(请输入下一句，输入错误将推出游戏)\n");
+	scanf("%s",&a);
+	if(strcmp(a,a2)==0)
+	{
+	printf("你可以开始接下来的游戏了,注意你只有10次机会");
+	for(q=0;q<10;q++)
+	{
+	scanf("%d",&i);
+	 if(i==o)
+		{
+			printf("恭喜回答正确,游戏结束");
+			q=10;
+		}
+	 else if(i>o)
+		{
+			 printf("太大了");
+		}
+	else if(i<o)
+		{
+			printf("太小了");
+		}
+	}
+	}
+}
